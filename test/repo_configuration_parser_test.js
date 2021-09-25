b9f06da49acc254e4ed02215c0926dcbec5e983f
Nahuel Garbezza <n.garbezza@gmail.com>
2021-09-25 06:51:07 -0300

    :white_check_mark: move env var parsing to a separate file and test it

---
+++ test/repo_configuration_parser_test.js
@@ -0,0 +1,37 @@
+const RepoConfigurationParser = require('../lib/repo_configuration_parser');
+const { suite, test, assert } = require('@pmoo/testy');
+
+suite('Repo configuration parser', () => {
+  test('it parses all variables starting with REPO_', () => {
+    const env = {
+      'NOT_APPLICABLE': 'foo',
+      'REPO_ABC': '{"name":"repoABC","org":"myOrg","includeLabels":[],"excludeLabels":[]}',
+      'REPO_XYZ': '{"name":"repoXYZ","org":"myOrg","includeLabels":[],"excludeLabels":[]}',
+    };
+    const parsedConfigurations = RepoConfigurationParser.parseFrom(env);
+    assert.that(parsedConfigurations).includesExactly(
+      { 'name': 'repoABC', 'org': 'myOrg', 'includeLabels': [], 'excludeLabels': [] },
+      { 'name': 'repoXYZ', 'org': 'myOrg', 'includeLabels': [], 'excludeLabels': [] },
+    );
+  });
+
+  test('it raises an error if a REPO_ variable has a syntax error', () => {
+    const env = {
+      'NOT_APPLICABLE': 'foo',
+      'REPO_ABC': '{"name":',
+    };
+    assert
+      .that(() => RepoConfigurationParser.parseFrom(env))
+      .raises(new Error(RepoConfigurationParser.errorMessageForVariableWithSyntaxErrors('REPO_ABC', '{"name":')));
+  });
+
+  test('it raises an error if a REPO_ variable can be parsed but it has missing fields', () => {
+    const env = {
+      'NOT_APPLICABLE': 'foo',
+      'REPO_ABC': '{"name":"repoABC","includeLabels":[],"excludeLabels":[]}', // missing 'org'
+    };
+    assert
+      .that(() => RepoConfigurationParser.parseFrom(env))
+      .raises(new Error(RepoConfigurationParser.errorMessageForMissingConfigurationFields('REPO_ABC')));
+  });
+});
